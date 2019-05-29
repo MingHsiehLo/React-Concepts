894bbc7e5a2e7abb41bd6f4ae7b061549b4fbe58
Yonatan Rojas S <yonatan@bedu.org>
2019-05-28 23:12:31 -0500

    finished ejemplo-01

--- Sesion06-React-router/ejemplo-01/src/App.js
+++ Sesion06-React-router/ejemplo-01/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 
-function App() {
+// Estos componentes deberían estar en su propio archivo, pero para simplificar
+// los pondemos aquí
+const Dashboard = props => <p>Dashboard</p>
+const CarritoDeCompras = props => <p>CarritoDeCompras</p>
+const Menu = props =>
+  <nav>
+    <ul>
+      <li>
+        { // Esta no es la manera correcta de hacerlo
+        }
+        <a href="/">Inicio</a>
+      </li>
+      <li>
+        { // Esta es la manera de usar Links (anchors) dentro de un router
+          // usando el mismo API que el router provee
+        }
+        <Link to="/carrito">Carrito de compras</Link>
+      </li>
+    </ul>
+  </nav>
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Menu />
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/carrito" component={CarritoDeCompras} />
+    </BrowserRouter>
+  )
 }
 
 export default App;
