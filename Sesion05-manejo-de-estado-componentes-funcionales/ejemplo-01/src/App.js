b444a670ca1ca8b69097f458e32dc63ed3504324
Yonatan Rojas S <yonatan@bedu.org>
2019-05-28 22:28:03 -0500

    finished ejemplo-01

--- Sesion05-manejo-de-estado-componentes-funcionales/ejemplo-01/src/App.js
+++ Sesion05-manejo-de-estado-componentes-funcionales/ejemplo-01/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(props) {
+  // Aquí definiremos el nombre con el que conoceremos el estado y la función
+  // con la que modificaremos dicho estado
+  const [count, setCount] = React.useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={() => setCount(count + 1)}>
+        Agregar uno
+      </button>
+      <p>La cuenta es: {count}</p>
+    </>
+  )
 }
 
 export default App;
